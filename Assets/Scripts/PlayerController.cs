4c41ec5b6454e9d88e4facc188d13a8f4145016b
Beck <splat.anonsvn@gmail.com>
2021-04-14 12:27:09 -0400

    Movement V2.01
    - Streamlined general movement code to permit for more dynamic tweaking.
    - Increased smoothness of movement.
    - Removed air control, since LIMBO doesn't have that.
    - PlayerController.cs overhauled with comments and new structure.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,58 +4,92 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public float speed;
-    public float jumpForce;
-    private float moveInput;
+    private Rigidbody2D myBody;
+    private Collider2D myCollider;
+    public LayerMask whatIsGround;
+    
+    public float footWidth; //how wide is our "foot" ray?
+    public float footDepth; //how far down is our "foot" ray?
+
+    public float speed; //speed multiplier
+    public float speedCap; //max speed
+    public float floorDrag; //how fast we should stop moving when touching the floor
+
+    public float jumpPower; //how much velocity we should apply to our player PER JUMP FRAME
+    public float jumpLength; //max jump length in frames
+
+    private float moveInput; //no idea yet
 
     public bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
-    public LayerMask whatIsGround;
-
-    private float jumpTimeCounter;
-    public float jumpTime;
-    public bool isJumping;
 
     SpriteRenderer myRenderer;
     public static bool faceRight = true;
 
 
-    // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
+        myBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        groundFinder();
+        leftRightMotionHandler();
     }
 
-    void Update(){
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+    void Update()
+    {
+        jumpHandler();
+        InputFinder();
+    }
+//----------------------------------------------------------------------------------------------------------------------
 
-        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
-            isJumping = true;
-            jumpTimeCounter = jumpTime;
-            rb.velocity = Vector2.up * jumpForce;
+    void groundFinder()
+    {
+        Physics2D.queriesStartInColliders = true;
+        Vector2 Xoffset = new Vector2(transform.position.x + (footWidth/2), transform.position.y - footDepth);  //this tells us what offset we use
+        RaycastHit2D floorDet = Physics2D.Raycast(Xoffset, new Vector2(-1, 0), footWidth, whatIsGround);
+        if(floorDet.collider != null)
+        {
+            Debug.Log(floorDet.collider.tag);
+            isGrounded = true;
+        } else{
+            isGrounded = false;
         }
 
-/*if(isJumping == false){
-        if(Input.GetKey(KeyCode.Space)){
-            if(jumpTimeCounter > 0){
- 
-                rb.velocity = Vector2.up * jumpForce;
-                jumpTimeCounter -= Time.deltaTime;
-            } else {
+        Debug.DrawRay(Xoffset, new Vector2(-footWidth, 0), Color.green);
+    }
 
-            }
+    void leftRightMotionHandler()
+    {
+        if(isGrounded)
+        {
+            myBody.velocity += new Vector2(moveInput * speed, 0);
         }
-    }*/
+        if(Mathf.Abs(myBody.velocity.x) > speedCap)
+        {
+            myBody.velocity = new Vector2(moveInput * speedCap, myBody.velocity.y);
+        }
+        if(moveInput == 0 && isGrounded)
+        {
+            myBody.velocity = new Vector2(myBody.velocity.x * floorDrag, myBody.velocity.y);
+        }
+    }
 
+    void jumpHandler()
+    {
+        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space))//fix this so it's more dynamic
+        {
+            isGrounded = false;
+            myBody.velocity = new Vector2(myBody.velocity.x, jumpPower); //temp
+        }
+    }
+
+    void InputFinder()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal"); //essentially positive or negative, pretty clever
     }
 }
