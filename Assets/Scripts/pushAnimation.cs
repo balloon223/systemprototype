b82e596415c1e59cc9973331ffb3f3d8f1d9a6df
Eva <yw4147@nyu.edu>
2021-04-25 17:49:13 +0800

    Add WaitToPush animation

---
+++ Assets/Scripts/pushAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pushAnimation : MonoBehaviour
+{
+    public Animator myAnim;
+    public bool detectPushableObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Pushable")){
+            detectPushableObj = true;
+            myAnim.SetBool("detectPushable", true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.CompareTag("Pushable")){
+            detectPushableObj = false;
+            myAnim.SetBool("detectPushable", false);
+        }
+    }
+}
