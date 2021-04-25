9f4c751d55bdf7af1bc94abf964dfd9d5e24d682
Eva <yw4147@nyu.edu>
2021-04-25 19:48:37 +0800

    Push animation added!

--- Assets/Scripts/pushAnimation.cs
+++ Assets/Scripts/pushAnimation.cs
@@ -6,18 +6,53 @@ public class pushAnimation : MonoBehaviour
 {
     public Animator myAnim;
     public bool detectPushableObj;
+    public bool collideWithObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        collideWithObject = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+        //    if(collideWithObject = true){
+ //               myAnim.SetBool("push", true);
+        //    }
+        //    else{
+        //        myAnim.SetBool("push", false);
+        //    }
+        }
+        else if(Input.GetKey("left") || Input.GetKey("right")){
+        //    if (collideWithObject = true){
+    //            myAnim.SetBool("push", true);
+        //    }
+        //    else{
+        //        myAnim.SetBool("push", false);
+        //    }
+        }
         
+
+        if (collideWithObject){
+            if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+                myAnim.SetBool("push", true);
+            }
+            else if(Input.GetKey("left") || Input.GetKey("right")){
+                myAnim.SetBool("push", true);
+            }
+            else {
+                myAnim.SetBool("push", false);
+            }
+        }
+        else {
+            myAnim.SetBool("push", false);
+        }
+
     }
+
+
     private void OnTriggerEnter2D(Collider2D other){
         if(other.CompareTag("Pushable")){
             detectPushableObj = true;
@@ -28,6 +63,20 @@ public class pushAnimation : MonoBehaviour
         if(other.CompareTag("Pushable")){
             detectPushableObj = false;
             myAnim.SetBool("detectPushable", false);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Pushable")
+        {
+            collideWithObject = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Pushable")
+        {
+           collideWithObject = false;
         }
     }
 }
