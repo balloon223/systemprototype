44935a87ab3fb7e21622b55abebdcb1de91bcc48
Bryan <sl6879@nyu.edu>
2021-05-04 15:42:25 +0800

    Added box sliding sfx

--- Assets/Scripts/CheckIfMoving.cs
+++ Assets/Scripts/CheckIfMoving.cs
@@ -4,40 +4,64 @@ using UnityEngine;
 
 public class CheckIfMoving : MonoBehaviour
 {
+    public PlayerController isWalking;
     AudioSource walkAudio;
     public float distance = 30f;
     public bool isMoving;
     public Transform playerCheckTransform;
-    public GameObject playerCheck;
     Vector2 direction = Vector2.right;
-    public float rayLength = 20f;
+    public float rayLength = 1.65f;
     public LayerMask playerLayerMask;
+    BoxCollider2D myCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         walkAudio = GetComponent<AudioSource>();
+        myCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-    }
-
-    void CheckPlayerInRange()    
-    {
-        Physics2D.queriesStartInColliders = false;
         RaycastHit2D playerHit = Physics2D.Raycast(playerCheckTransform.position, Vector2.left, rayLength, playerLayerMask);
 
         if (playerHit.collider != null)
         {
             isMoving=true;
+            if(!walkAudio.isPlaying){
+                walkAudio.Play();
+                }
         }
+        else
+        {
+            isMoving=false;
+            walkAudio.Stop();
+        }
+
+
     }
 
     void OnDrawGizmos(){
             Gizmos.color = Color.red;
             Gizmos.DrawLine(playerCheckTransform.position, new Vector2(playerCheckTransform.position.x - rayLength, playerCheckTransform.position.y));
+        }       
+
+/*    void CheckPlayerInRange()    
+    {
+        Physics2D.queriesStartInColliders = false;
+
+    }
+
+
+
+/*    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other != null && other.gameObject.tag == "Player"){
+            isMoving=true;
         }
+        else{
+            isMoving=false;
+        }
+    }   */
 }
