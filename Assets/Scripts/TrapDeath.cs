62406416776e30a37a43e2ca7733136fd71bb8d0
Eva <yw4147@nyu.edu>
2021-05-10 15:37:46 +0800

    Trap Death Animation added!!

---
+++ Assets/Scripts/TrapDeath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDeath : MonoBehaviour
+{
+    public Animator myAnim;
+
+    bool moveIntoTrap;
+
+    private TrapSelect trapSelector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveIntoTrap = false;
+
+        trapSelector = gameObject.GetComponent<TrapSelect>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trapSelector.getTrapped && !trapSelector.dead){
+            if (moveIntoTrap == false){
+                transform.position = new Vector2(transform.position.x+1, transform.position.y);
+                moveIntoTrap = true;
+            }
+            else{
+                myAnim.SetBool("trapDead", true);
+                
+            }
+        }
+        else{
+            myAnim.SetBool("trapDead", false);
+            moveIntoTrap = false;
+        }
+    }
+}
--- Assets/Scripts/TrapSelect.cs
+++ Assets/Scripts/TrapSelect.cs
@@ -10,11 +10,14 @@ public class TrapSelect : MonoBehaviour
     List<Animator> animatorList = new List<Animator>();
 
     public bool dead;
+    public bool getTrapped;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         dead = false;
+        getTrapped = false;
 
         if (trapList.Length >=1){
             for (int i = 0; i < trapList.Length; i++){
@@ -43,9 +46,11 @@ public class TrapSelect : MonoBehaviour
                         Debug.Log("same name");        
                         animatorList[i].enabled = true;
                         animatorList[i].SetBool("beingTrapped", true);
+                        getTrapped = true;
                     }
                     else{
                         animatorList[i].SetBool("beingTrapped", false);
+                        getTrapped = false;
                     }
                 }
             }
@@ -60,14 +65,15 @@ public class TrapSelect : MonoBehaviour
             string trapGameobjectName = trig.gameObject.name;
             Debug.Log(trapGameobjectName);
             FindTrap(trapGameobjectName);
+            //myAnim.SetBool("trapDead", true);
             StartCoroutine(waitForTrapDeath());
             //SceneManager.LoadScene(0);
         }
     }
 
     IEnumerator waitForTrapDeath(){
-        yield return new WaitForSeconds(1);
-        dead = true;
+        yield return new WaitForSeconds(1);        
+        dead = true;   
         SceneManager.LoadScene(0);
         dead = false;
     }
