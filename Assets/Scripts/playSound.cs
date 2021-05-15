4e5adafd5f5c54bfa1b6870b5dad8ecc926d733c
Bryan <sl6879@nyu.edu>
2021-05-15 15:48:49 +0800

    New sounds

---
+++ Assets/Scripts/playSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour
+{
+    public AudioClip triggerSound;
+    AudioSource audio;
+
+    void Start(){
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update(){
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(triggerSound != null && other.gameObject.tag == "Player"){
+            audio.PlayOneShot(triggerSound, 1);
+        }
+
+    }
+}
